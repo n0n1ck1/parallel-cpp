5364948648041ae6a70ded042316132378460909
n0n1ck <star_tyom@mail.ru>
2021-07-03 21:34:13 +0300

    solved hello_world

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -2,22 +2,33 @@
 
 #include <ostream>
 #include <string_view>
-
+#include <thread>
+#include <mutex>
+#include <vector>
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t /*n_threads*/)
+  HelloWorld(size_t n_threads)
   {
-    // Your code
+    n = n_threads;
   }
 
-  void SayHello(std::ostream& /*os*/) {
-    // Your code
+  void SayHello(std::ostream& os) {
+    std::mutex m;
+    for (size_t i = 0; i < n; ++i) {
+      threads.emplace_back([&]{
+        std::unique_lock<std::mutex> lock(m);
+        os << kHelloPrefix << std::this_thread::get_id() << '\n';
+      });
+    }
+    for (auto& t : threads) {
+      t.join();
+    }
   }
 
  private:
-  // Your code
+   size_t n;
+   std::vector<std::thread> threads;
 };
-
