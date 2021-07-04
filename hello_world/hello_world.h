e49182d91f41ba6749195412f7b6eedaa9cd959b
n0n1ck <star_tyom@mail.ru>
2021-07-04 18:59:31 +0300

    Revert "solved hello_world"
    This reverts commit 4160248116fb275d77122cb8b7067fd503c0fea2.

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -2,33 +2,22 @@
 
 #include <ostream>
 #include <string_view>
-#include <thread>
-#include <mutex>
-#include <vector>
+
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t n_threads)
+  HelloWorld(size_t /*n_threads*/)
   {
-    n = n_threads;
+    // Your code
   }
 
-  void SayHello(std::ostream& os) {
-    std::mutex m;
-    for (size_t i = 0; i < n; ++i) {
-      threads.emplace_back([&]{
-        std::unique_lock<std::mutex> lock(m);
-        os << kHelloPrefix << std::this_thread::get_id() << '\n';
-      });
-    }
-    for (auto& t : threads) {
-      t.join();
-    }
+  void SayHello(std::ostream& /*os*/) {
+    // Your code
   }
 
  private:
-   size_t n;
-   std::vector<std::thread> threads;
+  // Your code
 };
+
