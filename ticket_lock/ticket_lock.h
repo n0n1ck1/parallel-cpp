51fef89e9cb9402d4e1cf388923e0a5bb6d50d6d
n0n1ck <star_tyom@mail.ru>
2021-07-05 11:15:35 +0300

    fixed solution + style changes

--- ticket_lock/ticket_lock.h
+++ ticket_lock/ticket_lock.h
@@ -7,17 +7,18 @@ class TicketLock {
   }
 
   void Lock() {
-    my_ticket = next.exchange(next + 1);
-    while(my_ticket != now) {
+    std::atomic<uint64_t> my_ticket;
+    my_ticket = next_.fetch_add(1);
+    while(my_ticket != now_) {
       std::this_thread::yield();
     }
   }
 
   void Unlock() {
-    now.fetch_add(1);
+    now_.fetch_add(1);
   }
 
  private:
-  std::atomic<uint64_t> my_ticket;
-  std::atomic<uint64_t> now = 0, next = 0;
+  std::atomic<uint64_t> now_{0};
+  std::atomic<uint64_t> next_{0};
 };
