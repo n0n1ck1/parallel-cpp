31e13fe80c18ed9dbc1c416b7d909aee5f153621
n0n1ck <star_tyom@mail.ru>
2021-07-06 16:49:27 +0300

    solved deadlock

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -1,5 +1,9 @@
 #pragma once
+#include<mutex>
+#include<thread>
+#include<chrono>
 
+using namespace std::chrono_literals;
 
 class Deadlock {
  public:
@@ -7,14 +11,22 @@ class Deadlock {
   }
 
   void ThreadOne() {
-    // Your
+    left_fork_.lock();
+    std::this_thread::sleep_for(10ms);
+    right_fork_.lock();
+    left_fork_.unlock();
+    right_fork_.unlock();
   }
 
   void ThreadTwo() {
-    // Your
+    right_fork_.lock();
+    std::this_thread::sleep_for(10ms);
+    left_fork_.lock();
+    right_fork_.unlock();
+    left_fork_.unlock();
   }
 
  private:
-  // Your
+  std::mutex left_fork_;
+  std::mutex right_fork_;
 };
-
