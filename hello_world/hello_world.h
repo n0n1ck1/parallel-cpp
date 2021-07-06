fb19b228043e6f3452fd4fe22af13de0d9a7a505
n0n1ck <star_tyom@mail.ru>
2021-07-06 16:22:12 +0300

    actually fixed extra solution

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -15,10 +15,7 @@ class HelloWorld {
 
   void SayHello(std::ostream& os) {
     std::mutex mutex;
-<<<<<<< HEAD
     std::vector<std::thread> threads_;
-=======
->>>>>>> 55bae4b379ce954383f182b3a8e97f38bd4de1a5
     for (size_t i = 0; i < n_hellos_; ++i) {
       threads_.emplace_back([&]{
         std::unique_lock<std::mutex> lock(mutex);
@@ -32,8 +29,4 @@ class HelloWorld {
 
  private:
    const size_t n_hellos_;
-<<<<<<< HEAD
-=======
-   std::vector<std::thread> threads_;
->>>>>>> 55bae4b379ce954383f182b3a8e97f38bd4de1a5
 };
