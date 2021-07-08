4e244cf807ff10f257c8ad779f6f4da3f0bfdff8
n0n1ck <star_tyom@mail.ru>
2021-07-08 13:42:56 +0300

    doesn't work yet

--- rw_spin_lock/rw_spin_lock.h
+++ rw_spin_lock/rw_spin_lock.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include<atomic>
+#include<thread>
+#include<limits>
 
 class RWSpinLock {
  public:
@@ -7,22 +9,32 @@ class RWSpinLock {
   }
 
   void LockRead() {
-    // Your code
+    // 18 446 744 073 709 551 615 = std::numertic_limits<uint64_t> - 1
+    // this way i check if counter is even or not
+    while (std::atomic_fetch_or(&counter, 1) == counter) {
+      std::this_thread::yield();
+    }
+    counter += 2;
   }
 
   void UnlockRead() {
-    // Your code
+    counter -= 2;
   }
 
   void LockWrite() {
-    // Your code
+    while (std::atomic_fetch_or(&counter, 1) == counter) {
+      std::this_thread::yield();
+    }
+    counter += 1;
+    while (counter > 1) {
+      std::this_thread::yield();
+    }
   }
 
   void UnlockWrite() {
-    // Your code
+    counter -= 1;
   }
 
  private:
-  // Your code
+  std::atomic<uint64_t> counter = 0;
 };
-
