a2ebb9ed71a1c4e366ac793b6acc405bc245644d
n0n1ck <star_tyom@mail.ru>
2021-07-26 23:06:12 +0300

    split mutex_ into read_mutex_ and write_mutex_

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -11,18 +11,19 @@ class ThreadSafeVector {
   }
 
   T operator[](size_t index) const {
-    std::shared_lock lock(mutex_);
+    std::shared_lock lock(read_mutex_);
     return vector_[index];
   }
 
   size_t Size() const {
-    std::unique_lock lock(mutex_);
+    std::unique_lock lock(write_mutex_);
     return vector_.size();
   }
 
   void PushBack(const T& value) {
+    std::unique_lock lock(write_mutex_);
     if (vector_.capacity() == vector_.size()) {
-      std::unique_lock lock(mutex_);
+      std::unique_lock lock_read(read_mutex_);
       vector_.push_back(value);
       return;
     }
@@ -31,6 +32,7 @@ class ThreadSafeVector {
 
  private:
   // Your code
-  mutable std::shared_mutex mutex_;
+  mutable std::shared_mutex read_mutex_;
+  mutable std::mutex write_mutex_;
   std::vector<T> vector_;
 };
